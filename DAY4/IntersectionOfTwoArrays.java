fd758416bf4f9ee68f2c5caa04a5e77756bb4ef0
Shivani <missdnshivani@gmail.com>
2025-08-27 16:59:49 +0530

    DAy 4

--- DAY4/IntersectionOfTwoArrays.java
+++ DAY4/IntersectionOfTwoArrays.java
@@ -1,3 +1,28 @@
 //Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 //LEET CODE 350. Intersection of Two Arrays II
+
+class Solution {
+    public int[] intersect(int[] a, int[] b) {
+      int m=a.length;  
+      Arrays.sort(a);Arrays.sort(b); 
+      int n=b.length;
+      List<Integer> result=new ArrayList<>();
+      int i=0,j=0,k=0;
+      while(i<m&&j<n)
+      {
+       if(a[i]==b[j])
+       {result.add(a[i]);k++;i++;j++;}
+       else if (a[i]<b[j])
+       { i++;}
+       else
+       {j++;}
+      }
+      int[] r=new int[result.size()];
+      for(int q=0;q<result.size();q++)
+      {
+        r[q]=result.get(q);
+      }
+      return r;
+    }
+}
--- DAY4/MergeSort.java
+++ DAY4/MergeSort.java
@@ -1,2 +1,29 @@
 //You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 //LEET CODE 88. Merge Sorted Array
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+      int i=0,j=0,temp=0;
+      while(i<m&&j<n)
+      {
+      
+       if(nums1[i]<=nums2[j])
+       { i++; }
+      else 
+      {
+        for(int k=m;k>i;k--)
+        {nums1[k]=nums1[k-1];}
+        nums1[i]=nums2[j];
+        j++;
+        m++;
+        i++;
+      }
+    }
+      while(j<n)
+      {nums1[i]=nums2[j];
+      i++;
+      j++;
+      }
+
+    }
+}
